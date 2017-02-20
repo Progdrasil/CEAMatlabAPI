f638b1bf0e516c554d8878b4bba3da780a52fc76
Thorbl <game2world@yahoo.ca>
2017-02-20 17:20:47 -0500

    Wrough version of Wrapper works! Algorithm for reading output file is done, made it possible to write multiple fuels and oxidisers

--- CEA_Wrapper/ReadOutput.m
+++ CEA_Wrapper/ReadOutput.m
@@ -1,33 +1,58 @@
 function [ data ] = ReadOutput( fileStr )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+
+    %Debug variables
+%     clear all;
+%     close all;
+%     clc;
+%     currentpath = which('runWrapCEA.m');
+%     [pathstr,name,ext] = fileparts(currentpath);
+%     fileStr = strcat(pathstr,'/wrapper.dat');
+    
     IOout = fopen(fileStr,'r');
     tline = fgets(IOout) ;
-    disp(tline)
+    test = false;
+    perfo = false;
     while ischar(tline)
             
         %Finds the line containing the O/F number
-        if any(strfind(tline,'NOHARM(I)'),1)
-            disp(tline)
-            found = tline ;
-            ii = 0 ;
-            % Separates the line in strings
-            tempcell = strread(found, '%s', 'delimiter','=') ;
-            out.noharm = tempcell{end}(1) ;
+        if any(strfind(tline,'O/F'),1)
+            partstr = textscan(tline,'%s');
+            data.OF = str2num(partstr{end}{2}) ;
+            test = true;
+        elseif test
+            parts = textscan(tline,'%s');
+%             celldisp(parts)
+            if any(strfind(tline,'MOLE'),1)
+                test = false;
+            else
+                if  ~isempty(parts{1})
+                    if any(strfind(tline,'PERFORMANCE'),1)
+                            perfo = true;
+                    end
+                    if length(parts{1})>4
+                        if perfo
+                            sub = 2;
+                        else
+                            sub = 3;
+                        end
+                            
+                        N = length(parts{1}) -sub;
+                        parts{1}{1} = regexprep(parts{1}{1},'[,/()*]','');
+                        j = 1;
+                        for i = N:length(parts{1})
+                            data.(parts{1}{1})(j) = str2num(parts{1}{i});
+                            j = j+1;
+                        end
+                    end
+                end
+            end
         end
-
-%         if any(strfind(tline,'NO.          OMEGA          RAD./SEC.      HERTZ'),1)
-%             tline = fgets(fid);
-%             tline = fgets(fid);
-%             C_data = textscan(fileID,,'%d %f %f %f');
-%             C_data(1);
-%         end
-
         tline = fgets(IOout);
-        disp(tline)
     end
 
-    data = fclose(IOout);
+    fclose(IOout);
     
 end
 
